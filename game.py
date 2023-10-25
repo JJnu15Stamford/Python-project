dc5cae24e05311aa6f4db982f5908965547461e9
SA48 <sa48@Skills-Academy-48.local>
2023-10-25 18:24:46 -0400

    I am pushing my first game to Github. It also had my flowchart and algorithm

---
+++ game.py
@@ -0,0 +1,33 @@
+print("choose your character")
+
+theCharacter = ["john vega", "jacky brown", "wendy smith"]
+print(theCharacter[0])
+print(theCharacter[1])
+print(theCharacter[2])
+
+option = int(input("enter 1 for john vega, enter 2 for jacky brown, enter 3 for wendy smith: "))
+
+if option == 1:
+    print("hey there john vega")
+elif option == 2:
+    print("hey there jacky brown")
+elif option == 3: 
+    print("hey there wendy smith")
+else:
+    print("invalid input, please types 1,2, or 3")
+
+print("john vega hear strange noises around the house and then he hears laughing and knocking in the basement")
+sOption = int(input("type 1 to run away, type 2 to check it out: "))
+if option == 1:
+    print("you find 3 doors")
+    print("which door will you choose")
+    firstPathOption = ("enter 1, for first door, enter 2 for sencond door, enter 3 for third door")
+    if option 
+elif option == 2:
+    print("you find nothing")
+else:
+    print("invalid input, please types 1 or 2")
+
+print("end")
+
+print("")
